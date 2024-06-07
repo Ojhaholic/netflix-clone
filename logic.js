de17dc5e007166260ab791f56a39ec2ecbce0ceb
Devansh Ojha <157592995+Ojhaholic@users.noreply.github.com>
2024-06-07 16:05:50 +0530

    Update logic.js

---
+++ logic.js
@@ -0,0 +1,23 @@
+qna1 = document.body.querySelector(".qna1").addEventListener("click", ()=>{
+    document.body.querySelector(".a1").classList.toggle("aq1")    
+})
+
+qna2 = document.body.querySelector(".qna2").addEventListener("click", ()=>{
+    document.body.querySelector(".a2").classList.toggle("aq2")    
+})
+
+qna3 = document.body.querySelector(".qna3").addEventListener("click", ()=>{
+    document.body.querySelector(".a3").classList.toggle("aq3")    
+})
+
+qna4 = document.body.querySelector(".qna4").addEventListener("click", ()=>{
+    document.body.querySelector(".a4").classList.toggle("aq4")    
+})
+
+qna5 = document.body.querySelector(".qna5").addEventListener("click", ()=>{
+    document.body.querySelector(".a5").classList.toggle("aq5")    
+})
+
+qna6 = document.body.querySelector(".qna6").addEventListener("click", ()=>{
+    document.body.querySelector(".a6").classList.toggle("aq6")    
+})
